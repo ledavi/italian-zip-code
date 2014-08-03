62bcb5c904101ebad5b27e74e0f8f9a514804120
maxime.68@gmail.com <maxime.68@gmail.com@0dfcc416-4e19-0e7a-2be3-59d6a783b285>
2014-08-03 17:29:34 +0000

    project checkout

---
+++ src/it_it/Zipcode.java
@@ -0,0 +1,15 @@
+package it_it;
+
+public class Zipcode {
+
+	private static final String GOOGLE_GEOCODING_WS_URL = "http://maps.googleapis.com/maps/api/geocode/";
+	
+	public static enum OUTPUT_FORMAT{json, xml};
+	
+	private static final String KEY = "AIzaSyAjb9a3m_6bmh3yIynW7Yr6OY9nuc4NomY";
+	
+	public Zipcode() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
